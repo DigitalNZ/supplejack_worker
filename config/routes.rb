019ab447db3e881bbf4d310461f95a0b54d182ae
Michael Treacher <michael.treacher@boost.co.nz>
2013-06-10 10:32:28 +1200

    Changed route file to allow for index on harvest jobs

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ HarvesterWorker::Application.routes.draw do
   devise_for :users, skip: :sessions
 
   resources :abstract_jobs, only: [:index]
-  resources :harvest_jobs, only: [:create, :update, :show]
+  resources :harvest_jobs, only: [:create, :update, :show, :index]
   resources :enrichment_jobs, only: [:create, :update, :show]
   resources :harvest_schedules, only: [:index, :create, :update, :show, :destroy]
   resources :previews, only: [:create]
