942937a828a627c728c1606e8f934b033f4a829a
Andy Gray <andy.gray@boost.co.nz>
2013-07-29 11:53:38 +1200

    Changed api deletions to use puts

--- app/workers/api_delete_worker.rb
+++ app/workers/api_delete_worker.rb
@@ -3,7 +3,7 @@ class ApiDeleteWorker
 
 	def perform(identifier, job_id)
 		job = AbstractJob.find(job_id)
-		RestClient.delete("#{ENV["API_HOST"]}/harvester/records/#{identifier}", content_type: :json, accept: :json)
+		RestClient.put("#{ENV["API_HOST"]}/harvester/records/delete", {id: identifier}, {content_type: :json, accept: :json})
 		job.inc(:posted_records_count, 1)
 	end
 end
