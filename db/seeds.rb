073a1afb36a5992897e1c577a5166262c15c26c4
Andy Gray <mr.andy.gray@gmail.com>
2013-08-08 16:44:02 +1200

    STORY [53588425] HARVEST ADDITIONAL SOURCES: As a harvest operator I want to be able to add additional sources to a record in a harvest

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(authentication_token: 'CcfouGYmkBnRL71aqAp8')
