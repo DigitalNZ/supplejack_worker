16b2f17fd07c5980b28a1bb58b20d488c408a34a
Andy Gray <andy.gray@boost.co.nz>
2013-08-13 11:44:03 +1200

    Revert "FIX US [#54063945] CHECK COLLECTIONS"
    This reverts commit 73877131719fcb75d3ec5191b651d740e6adbbec.

--- spec/workers/collection_check_worker_spec.rb
+++ spec/workers/collection_check_worker_spec.rb
@@ -5,8 +5,7 @@ describe CollectionCheckWorker do
   let(:worker) { CollectionCheckWorker.new }
 
   before(:each) do
-    worker.instance_variable_set(:@primary_collection, 'Carterton Kete')
-    worker.instance_variable_set(:@escaped_primary_collection,'Carterton%20Kete')
+    worker.instance_variable_set(:@primary_collection,'TAPUHI')
   end
 
   describe "#perform" do
@@ -16,34 +15,17 @@ describe CollectionCheckWorker do
       worker.stub(:suppress_collection)
     end
 
-    context "setup" do
-      before(:each) do
-        worker.stub(:collection_records) { [] }
-      end
-
-      it "should set the primary_collection instance variable" do
-        worker.perform('Carterton Kete')
-        worker.instance_variable_get(:@primary_collection).should eq 'Carterton Kete'
-      end
-
-      it "should escape the primary_collection as it's used in URLS" do
-        ERB::Util.should_receive(:url_encode).with('Carterton Kete') { 'Carterton%20Kete' }
-        worker.perform('Carterton Kete')
-        worker.instance_variable_get(:@escaped_primary_collection).should eq 'Carterton%20Kete'
-      end
-    end
-
     it "should retrieve landing urls from the API to check" do
       worker.stub(:up?) {true}
       worker.should_receive(:collection_records) { @records }
-      worker.perform('Carterton Kete')
+      worker.perform('TAPUHI')
     end
 
     it "should check that the records are up" do
       worker.stub(:collection_records) { @records }
       worker.should_receive(:up?).with('http://google.com/1')
       worker.should_receive(:up?).with('http://google.com/2')
-      worker.perform('Carterton Kete')
+      worker.perform('TAPUHI')
     end
 
     context "the collection is active and all links are down" do
@@ -56,7 +38,7 @@ describe CollectionCheckWorker do
 
       it "should add the collection to the blacklist" do
         worker.should_receive(:suppress_collection)
-        worker.perform('Carterton Kete')
+        worker.perform('TAPUHI')
       end
     end
 
@@ -70,7 +52,7 @@ describe CollectionCheckWorker do
 
       it "should remove the collection from the blacklist" do
         worker.should_receive(:activate_collection)
-        worker.perform('Carterton Kete')
+        worker.perform('TAPUHI')
       end
     end
   end
@@ -81,11 +63,11 @@ describe CollectionCheckWorker do
 
     before do
       JSON.stub(:parse) { [] }
-      RestClient.stub(:get).with("#{ENV['API_HOST']}/link_checker/collection_records/Carterton%20Kete") { response }
+      RestClient.stub(:get).with("#{ENV['API_HOST']}/link_checker/collection_records/TAPUHI") { response }
     end
 
     it "should retrieve landing urls from the API to check" do
-      RestClient.should_receive(:get).with("#{ENV['API_HOST']}/link_checker/collection_records/Carterton%20Kete") { response }
+      RestClient.should_receive(:get).with("#{ENV['API_HOST']}/link_checker/collection_records/TAPUHI") { response }
       worker.send(:collection_records)
     end
 
@@ -101,7 +83,7 @@ describe CollectionCheckWorker do
     end
 
     it "should retrieve the collections status" do
-      RestClient.should_receive(:get).with("#{ENV['API_HOST']}/link_checker/collections/Carterton%20Kete")
+      RestClient.should_receive(:get).with("#{ENV['API_HOST']}/link_checker/collections/TAPUHI")
       worker.send(:collection_active?)
     end
 
@@ -140,7 +122,7 @@ describe CollectionCheckWorker do
     
     it "gets the url and validates it" do
       worker.should_receive(:get).with('http://blah.com') { response }
-      worker.should_receive(:validate_collection_rules).with(response, 'Carterton Kete') { true }
+      worker.should_receive(:validate_collection_rules).with(response, 'TAPUHI') { true }
       worker.send(:up?,'http://blah.com').should be_true
     end
   end
@@ -149,17 +131,17 @@ describe CollectionCheckWorker do
 
     before do
       RestClient.stub(:put)
-      CollectionMailer.stub(:collection_status).with("Carterton Kete", "down")
+      CollectionMailer.stub(:collection_status).with("TAPUHI", "down")
     end
 
     it "should suppress the collection" do
-      RestClient.should_receive(:put).with("#{ENV['API_HOST']}/link_checker/collections/Carterton%20Kete", {status: 'suppressed'})
+      RestClient.should_receive(:put).with("#{ENV['API_HOST']}/link_checker/collections/TAPUHI", {status: 'suppressed'})
       worker.send(:suppress_collection)
     end
 
     it "should send an email that the collection is down" do
       worker.send(:suppress_collection)
-      expect(CollectionMailer).to have_received(:collection_status).with("Carterton Kete", "down")
+      expect(CollectionMailer).to have_received(:collection_status).with("TAPUHI", "down")
     end
   end
 
@@ -167,17 +149,17 @@ describe CollectionCheckWorker do
 
     before do
       RestClient.stub(:put)
-      CollectionMailer.stub(:collection_status).with("Carterton Kete", "up")
+      CollectionMailer.stub(:collection_status).with("TAPUHI", "up")
     end
 
     it "should suppress the collection" do
-      RestClient.should_receive(:put).with("#{ENV['API_HOST']}/link_checker/collections/Carterton%20Kete", {status: 'active'})
+      RestClient.should_receive(:put).with("#{ENV['API_HOST']}/link_checker/collections/TAPUHI", {status: 'active'})
       worker.send(:activate_collection)
     end
 
     it "should send an email that the collection is down" do
       worker.send(:activate_collection)
-      expect(CollectionMailer).to have_received(:collection_status).with("Carterton Kete", "up")
+      expect(CollectionMailer).to have_received(:collection_status).with("TAPUHI", "up")
     end
   end
 end
