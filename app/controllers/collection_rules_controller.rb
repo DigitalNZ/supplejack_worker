08d6a8f91c4f44016e2a60cc5e0df1e6a66e1867
Benedict Aluan <benedict.aluan@gmail.com>
2013-08-27 15:06:26 +1200

    Made the CollectionRules controller respond with JSON

--- app/controllers/collection_rules_controller.rb
+++ app/controllers/collection_rules_controller.rb
@@ -1,6 +1,6 @@
 class CollectionRulesController < ApplicationController
 
-  respond_to :json, :html
+  respond_to :json
 
   def index
     @collection_rules = params[:collection_rules].present? ? CollectionRules.where(params[:collection_rules]) : CollectionRules.all
@@ -12,35 +12,19 @@ class CollectionRulesController < ApplicationController
     respond_with @collection_rule
   end
 
-  def new
-    @collection_rule = CollectionRules.new
-  end
-
-  def edit
-    @collection_rule = CollectionRules.find(params[:id])
-  end
-
   def destroy
     @collection_rule = CollectionRules.find(params[:id])
-    @collection_rule.destroy
-    redirect_to collection_rules_path
+    respond_with @collection_rule.destroy
   end
 
   def create
-    @collection_rule = CollectionRules.new(params[:collection_rules])
-    if @collection_rule.save
-      redirect_to collection_rules_path
-    else
-      render :new
-    end
+    @collection_rule = CollectionRules.create(params[:collection_rules])
+    respond_with @collection_rule
   end
 
   def update
     @collection_rule = CollectionRules.find(params[:id])
-    if @collection_rule.update_attributes(params[:collection_rules])
-      redirect_to collection_rules_path
-    else
-      render :edit
-    end
+    @collection_rule.update_attributes(params[:collection_rules])
+    respond_with @collection_rule
   end
 end
--- spec/controllers/collection_rules_controller_spec.rb
+++ spec/controllers/collection_rules_controller_spec.rb
@@ -32,42 +32,11 @@ describe CollectionRulesController do
     end
   end
 
-  describe "GET 'new'" do
-    it "creates a new collection rule" do
-      CollectionRules.should_receive(:new) { collection_rule }
-      get :new
-      assigns(:collection_rule).should eq collection_rule
-    end
-  end
-
-  describe "GET 'edit'" do
-    it "finds the collection_rule" do
-      CollectionRules.should_receive(:find) { collection_rule }
-      get :edit, id: collection_rule.id
-      assigns(:collection_rule).should eq collection_rule
-    end
-  end
-
   describe "POST 'create'" do
     it "should make a new collection rule and assign it" do
-      CollectionRules.should_receive(:new) { collection_rule }
-      post :create, collection_rule: { collection_title: "TAPUHI", status_codes: "203,205" }
+      CollectionRules.should_receive(:create).with({ "collection_title" => "TAPUHI", "status_codes" => "203,205" }) { collection_rule }
+      post :create, collection_rules: { collection_title: "TAPUHI", status_codes: "203,205" }
       assigns(:collection_rule) { collection_rule }
-    end
-
-    it "should redirect_to the index page" do
-      CollectionRules.stub(:new) { collection_rule }
-      post :create, collection_rule: { collection_title: "TAPUHI", status_codes: "203,205" }
-      response.should redirect_to collection_rules_path
-    end
-
-    context "collection_rule not valid" do
-      it "should render the new template" do
-        CollectionRules.stub(:new) { collection_rule }
-        collection_rule.stub(:save) { false }
-        post :create, collection_rule: { collection_title: "TAPUHI", status_codes: "203,205" }
-        response.should render_template(:new)
-      end
     end
   end
 
@@ -78,25 +47,10 @@ describe CollectionRulesController do
       assigns(:collection_rule) { collection_rule }
     end
 
-    it "should redirect_to the index path" do
-      CollectionRules.stub(:find) { collection_rule }
-      put :update, id: collection_rule.id, collection_rule: { collection_title: "TAPUHI", status_codes: "203,205" }
-      response.should redirect_to collection_rules_path
-    end
-
     it "updates all the attributes" do
       CollectionRules.stub(:find) { collection_rule }
       collection_rule.should_receive(:update_attributes).with({"collection_title" => "TAPUHI", "status_codes" => "203,205" })
       put :update, id: collection_rule.id, collection_rules: { collection_title: "TAPUHI", status_codes: "203,205" }
-    end
-
-    context "collection_rule not valid" do
-      it "should render the edit template" do
-        CollectionRules.stub(:find) { collection_rule }
-        collection_rule.stub(:update_attributes) { false }
-        post :update, id: collection_rule.id, collection_rules: { collection_title: "TAPUHI", status_codes: "203,205" }
-        response.should render_template(:edit)
-      end
     end
   end
 
@@ -104,7 +58,6 @@ describe CollectionRulesController do
     it "finds the collection rule and destroys it" do
       CollectionRules.should_receive(:find) { collection_rule }
       delete :destroy, id: collection_rule.id
-      response.should redirect_to collection_rules_path
     end
   end
 
