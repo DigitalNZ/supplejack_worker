d7b32b6025bb426cabfca7429d4765fd9a9974f2
Benedict Aluan <benedict.aluan@gmail.com>
2013-08-27 14:56:10 +1200

    Moved the collection rules model over to the worker.

--- app/models/collection_rules.rb
+++ app/models/collection_rules.rb
@@ -1,6 +1,12 @@
-class CollectionRules < ActiveResource::Base
+class CollectionRules
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :collection_title, type: String
+  field :xpath, type: String
+  field :status_codes, type: String
+  field :active, type: Boolean, default: true
+  field :throttle, type: Integer
 
-  self.site = ENV['MANAGER_HOST']
-  self.user = ENV['MANAGER_API_KEY']
-
+  validates :collection_title, presence: true, uniqueness: true 
 end
