d76a2b80fa92548f62500f86551e7099cba6c740
Benedict Aluan <benedict.aluan@gmail.com>
2014-04-03 14:00:12 +1300

    SIFTER [#6528] Updated Ignored list in Airbrake.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,13 @@
 Airbrake.configure do |config|
   config.api_key = ENV['AIRBRAKE_API_KEY']
 
-  # Ignore an error In addition to The defaults
-  config.ignore << 'ThrottleLimitError'
+  config.ignore_only = ['ActiveRecord::RecordNotFound',
+                      'ActionController::RoutingError',
+                      'ActionController::InvalidAuthenticityToken',
+                      'CGI::Session::CookieStore::TamperedWithCookie',
+                      'ActionController::UnknownHttpMethod',
+                      'ActionController::UnknownAction',
+                      'AbstractController::ActionNotFound',
+                      'Mongoid::Errors::DocumentNotFound',
+                      'ThrottleLimitError']
 end
