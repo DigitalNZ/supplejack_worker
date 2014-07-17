644f197b6541e3a9d3347855ce72bc06cb54f4eb
Benedict Aluan <benedict@boost.co.nz>
2014-07-18 11:35:19 +1200

    Ignore compacting of multi-value fields.

--- spec/models/matcher/concept_matcher_spec.rb
+++ spec/models/matcher/concept_matcher_spec.rb
@@ -9,7 +9,8 @@ describe Matcher::ConceptMatcher do
   let(:attributes) { {priority: 0, source_id: 'mccahon_co_nz', match_concepts: :create_or_match, 
                       internal_identifier: 'http://www.mccahon.co.nz/',
                       givenName: 'Colin', familyName: 'McCahon',
-                      dateOfBirth: DateTime.parse('1919-01-01'), dateOfDeath: DateTime.parse('1987-01-01')} }
+                      dateOfBirth: DateTime.parse('1919-01-01'), dateOfDeath: DateTime.parse('1987-01-01'),
+                      sameAs: ['http://www.en.wikipedia.com/mccahon']} }
   
   let(:fragment) { SupplejackApi::ApiConcept::ConceptFragment.new(attributes) }
   let(:concept) { SupplejackApi::Concept.create(landing_url: 'http://www.mccahon.co.nz/', internal_identifier: 'http://www.mccahon.co.nz/', status: 'active') }
@@ -75,7 +76,7 @@ describe Matcher::ConceptMatcher do
 
       it "should post an update to the API with the sameAs and match_status fields" do
         worker.send(:lookup, attributes)
-        expect(ApiUpdateWorker).to have_enqueued_job("/harvester/concepts.json", {"concept"=>{"internal_identifier"=>"http://www.mccahon.co.nz/", "source_id" => "mccahon_co_nz", "landing_url"=>"http://www.mccahon.co.nz/", "sameAs"=>"http://www.mccahon.co.nz/", "match_status"=>"strong"}}, nil)
+        expect(ApiUpdateWorker).to have_enqueued_job("/harvester/concepts.json", {"concept"=>{"internal_identifier"=>"http://www.mccahon.co.nz/", "source_id" => "mccahon_co_nz", "landing_url"=>"http://www.mccahon.co.nz/", "sameAs"=>["http://www.en.wikipedia.com/mccahon"], "match_status"=>"strong"}}, nil)
       end
     end
   end
