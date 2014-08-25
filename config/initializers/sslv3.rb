9ea542730248f233dd9c3cca2280de73d7d0b64f
Dave Harris <dave@boost.co.nz>
2014-08-25 16:54:12 +1200

    Revert "Set SSL connections to use SSLv3"
    This reverts commit 8fda9048265b2238db3a6acb8e91d66e8f25f32f.

--- config/initializers/sslv3.rb
+++
@@ -1,8 +0,0 @@
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3. 
-# See https://github.com/DigitalNZ/supplejack_worker for details. 
-# 
-# Supplejack was created by DigitalNZ at the National Library of NZ
-# and the Department of Internal Affairs. http://digitalnz.org/supplejack
-
-OpenSSL::SSL::SSLContext::DEFAULT_PARAMS[:ssl_version] = 'SSLv3'
