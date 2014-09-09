862951901e8e1a226f1f562cafa759f87a6bbd42
Dave Harris <dave@boost.co.nz>
2014-09-10 10:26:48 +1200

    BUG [#6634] Harvests keep going in the background after being stopped
    - Update AbstractWorker#stop_harvest? to include 'finished' state

--- app/workers/api_update_worker.rb
+++ app/workers/api_update_worker.rb
@@ -5,25 +5,23 @@
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
-class ApiUpdateWorker
+class ApiUpdateWorker < AbstractWorker
 	include Sidekiq::Worker
 
 	def perform(path, attributes, job_id)
-		job = AbstractJob.find(job_id)
+    @job = AbstractJob.find(job_id)
+    return if self.stop_harvest?
 
-		attributes.merge!(preview: true) if job.environment == "preview"
+		attributes.merge!(preview: true) if @job.environment == "preview"
 
 		measure = Benchmark.measure do
-      Sidekiq.logger.info "POST: #{ENV["API_HOST"]}#{path}"
-      Sidekiq.logger.info "Parameters: #{attributes.to_json}"
-
       response = RestClient.post "#{ENV["API_HOST"]}#{path}", attributes.to_json, content_type: :json, accept: :json
       response = JSON.parse(response)
-      job.set(last_posted_record_id: response["record_id"])
+      @job.set(last_posted_record_id: response["record_id"])
     end
 
-    job.inc(:posted_records_count => 1)
+    @job.inc(:posted_records_count => 1)
 
-    Sidekiq.logger.info "POST #{job.class} #{job.environment.capitalize} to #{path}. Time: #{measure.real.round(4)}s"
+    Sidekiq.logger.info "POST #{@job.class} #{@job.environment.capitalize} to #{path}. Time: #{measure.real.round(4)}s"
 	end
 end
