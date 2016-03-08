1822615b73ec30a622e999549c01ba48da42ce14
Reinkaos <rj.matthews23@gmail.com>
2016-03-08 13:41:21 +1300

    Update schedule.rb to stagger the cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,31 +1,32 @@
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3. 
-# See https://github.com/DigitalNZ/supplejack_worker for details. 
-# 
+# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
+# and is licensed under the GNU General Public License, version 3.
+# See https://github.com/DigitalNZ/supplejack_worker for details.
+#
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
-if @enviroment == "development"
-  env :PATH, ENV['PATH']
+env :PATH, ENV['PATH'] if @enviroment == 'development'
+
+every :day, at: '2:00am' do
+  runner 'HarvestJob.clear_raw_data'
 end
 
-every :day, at: "2:00am" do
-  runner "HarvestJob.clear_raw_data"
-end
-
-every 5.minutes do
-  runner "HarvestSchedule.create_one_off_jobs"
-  runner "HarvestSchedule.create_recurrent_jobs"
-end
-
-every 1.day, :at => '6:00 am' do
-  runner "CollectionStatistics.email_daily_stats"
+every 3.minutes do
+  runner 'HarvestSchedule.create_one_off_jobs'
 end
 
 every 5.minutes do
-  runner "NetworkChecker.check"
+  runner 'NetworkChecker.check'
+end
+
+every 7.minutes do
+  runner 'HarvestSchedule.create_recurrent_jobs'
+end
+
+every 1.day, at: '6:00 am' do
+  runner 'CollectionStatistics.email_daily_stats'
 end
 
 every 2.hours do
-  runner "EnqueueSourceChecksWorker.perform_async"
+  runner 'EnqueueSourceChecksWorker.perform_async'
 end
