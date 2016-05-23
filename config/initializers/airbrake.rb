a910a961168d47ac1cbcf95ef9ae10adf73f6c1d
Reinkaos <rj.matthews23@gmail.com>
2016-05-24 09:39:48 +1200

    Guard Airbrake filter

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -5,16 +5,20 @@
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
-Airbrake.configure do |config|
-  config.project_key = ENV['AIRBRAKE_PROJECT_API_KEY']
-  config.project_id = ENV['AIRBRAKE_PROJECT_ID']
-  config.ignore_environments = %w(development test)
-end
-
-Airbrake.add_filter do |notice|
-  # The library supports nested exceptions, so one notice can carry several
-  # exceptions.
-  if notice[:errors].any? { |error| error[:type] == 'ThrottleLimitError' }
-    notice.ignore!
+if ENV['AIRBRAKE_PROJECT_ID']
+  Airbrake.configure do |config|
+    config.project_key = ENV['AIRBRAKE_PROJECT_API_KEY']
+    config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    config.ignore_environments = %w(development test)
   end
+
+  Airbrake.add_filter do |notice|
+    # The library supports nested exceptions, so one notice can carry several
+    # exceptions.
+    if notice[:errors].any? { |error| error[:type] == 'ThrottleLimitError' }
+      notice.ignore!
+    end
+  end
+else
+  Rails.logger.debug 'The AIRBRAKE_PROJECT_ID ENV Variable has not been set'
 end
