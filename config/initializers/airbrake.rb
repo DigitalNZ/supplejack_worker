0baf83dba4312dc29738ae84518dbd6dae12d738
Reinkaos <rj.matthews23@gmail.com>
2016-05-24 09:12:43 +1200

    Update airbrake to the new ignore syntax

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -9,5 +9,12 @@ Airbrake.configure do |config|
   config.project_key = ENV['AIRBRAKE_PROJECT_API_KEY']
   config.project_id = ENV['AIRBRAKE_PROJECT_ID']
   config.ignore_environments = %w(development test)
-  config.ignore << 'ThrottleLimitError'
+end
+
+Airbrake.add_filter do |notice|
+  # The library supports nested exceptions, so one notice can carry several
+  # exceptions.
+  if notice[:errors].any? { |error| error[:type] == 'ThrottleLimitError' }
+    notice.ignore!
+  end
 end
