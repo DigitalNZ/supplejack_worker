4242882bdc326d525d05c5a286e96b2ca847e442
Reinkaos <rj.matthews23@gmail.com>
2016-05-23 13:20:43 +1200

    Update airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,12 +1,13 @@
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3. 
-# See https://github.com/DigitalNZ/supplejack_worker for details. 
-# 
+# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
+# and is licensed under the GNU General Public License, version 3.
+# See https://github.com/DigitalNZ/supplejack_worker for details.
+#
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
 Airbrake.configure do |config|
-  config.api_key = ENV['AIRBRAKE_API_KEY']
-
+  config.project_key = '55ec146de217fb5c05d235469a4d9279'
+  config.project_id = '96483'
+  config.ignore_environments = %w(development test)
   config.ignore << 'ThrottleLimitError'
 end
