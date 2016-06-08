dc1d1d757ae81f5eb0cbad66b0b76804c9cec334
Reinkaos <rj.matthews23@gmail.com>
2016-06-08 13:33:58 +1200

    add environment to worker

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -9,6 +9,7 @@ if ENV['AIRBRAKE_PROJECT_ID']
   Airbrake.configure do |config|
     config.project_key = ENV['AIRBRAKE_PROJECT_API_KEY']
     config.project_id = ENV['AIRBRAKE_PROJECT_ID']
+    config.environment = Rails.env
     config.ignore_environments = %w(development test)
   end
 
