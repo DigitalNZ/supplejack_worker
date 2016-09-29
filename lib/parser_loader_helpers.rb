7fbf5b1a8e1af49b468de8cf0d92f45de54f04ac
Jeffery <jeffery@boost.co.nz>
2016-09-29 13:06:11 +1300

    fix the method of loading parser

--- lib/parser_loader_helpers.rb
+++ lib/parser_loader_helpers.rb
@@ -8,15 +8,25 @@
 module ParserLoaderHelpers
   attr_reader :loader
 
+  # Return a parser based on the latest version within specific environment
+  # @param environment of the parser that needs to be loaded
+  # @author Jeffery Liang
   def load_file(environment)
+    SupplejackCommon::Oai::Base.clear_definitions
+    # The SupplejackCommon::Oai::Base include a cache-alike mechanism to store options' container with (identifiter => set) 
+    # invoking clear_definition will clear such attributes "set" in the base class
     @loader ||= SupplejackCommon::Loader.new(self, environment)
     loader.load_parser
   end
 
+  # Return a definition based on the latest parser version within specific environment
+  # @param environment of the parser whose enrichment is requested
+  # @author Jeffery Liang
   def enrichment_definitions(environment)
     return @enrichment_definitions if @enrichment_definitions
+    # the same reason that we use clear_definition in the load_file method
+    SupplejackCommon::Oai::Base.clear_definitions
     @loader ||= SupplejackCommon::Loader.new(self, environment)
-
     if loader.loaded?
       @enrichment_definitions = loader.parser_class.enrichment_definitions
     end
