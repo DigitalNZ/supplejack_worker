a4a6908fea38ac26f0eb5f11c227997b0a4103cd
Benedict Aluan <benedict@boost.co.nz>
2016-10-27 17:11:14 +1300

    Add Docker seed task

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 require 'resolv-replace'
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_HOST'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_HOST'] }
+  config.redis = { url: ENV['REDIS_URL'] }
 end
