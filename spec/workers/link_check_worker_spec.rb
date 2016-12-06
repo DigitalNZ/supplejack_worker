ffbe64be2389376a8e9b6f9b0feb7df85b67169d
Edwin Rozario <edwin@boost.co.nz>
2016-12-06 15:18:02 +1300

    Updated all the old specs for LinkCheckWorker

--- spec/workers/link_check_worker_spec.rb
+++ spec/workers/link_check_worker_spec.rb
@@ -103,7 +103,7 @@ describe LinkCheckWorker do
             expect { worker.perform(link_check_job.id.to_s) }.to_not raise_exception
           end
           
-          it "should handle networking errors" do
+          it 'should handle networking errors' do
             worker.stub(:link_check).and_raise(StandardError.new('RestClient Exception'))
             expect {worker.perform(link_check_job.id.to_s)}.to_not raise_exception
           end
@@ -181,136 +181,111 @@ describe LinkCheckWorker do
     end
   end
 
-  # describe "add_record_stats" do
+  describe 'add_record_stats' do
+    let(:collection_statistics) { double(:collection_statistics) }
 
-  #   let(:collection_statistics) { double(:collection_statistics) }
+    before do
+      worker.stub(:link_check_job) { link_check_job }
+      worker.stub(:collection_stats) { collection_statistics }
+    end
 
-  #   before do
-  #     worker.stub(:link_check_job) { link_check_job }
-  #     worker.stub(:collection_stats) { collection_statistics }
-  #   end
+    it 'should add record stats for deleted' do
+      expect(collection_statistics).to receive(:add_record!).with(12345, 'deleted', 'http://google.co.nz')
+      worker.send(:add_record_stats, 12345, 'deleted')
+    end
 
-  #   it "should add record stats for 'deleted'" do
-  #     collection_statistics.should_receive(:add_record!).with(12345, 'deleted', "http://google.co.nz")
-  #     worker.send(:add_record_stats,12345, 'deleted')
-  #   end
+    it 'should add record stats for suppressed' do
+      expect(collection_statistics).to receive(:add_record!).with(12345, 'suppressed', 'http://google.co.nz')
+      worker.send(:add_record_stats, 12345, 'suppressed')
+    end
 
-  #   it "should add record stats for 'suppressed'" do
-  #     collection_statistics.should_receive(:add_record!).with(12345, 'suppressed', "http://google.co.nz")
-  #     worker.send(:add_record_stats,12345, 'suppressed')
-  #   end
+    it 'should add record stats for active' do
+      expect(collection_statistics).to receive(:add_record!).with(12345, 'activated', 'http://google.co.nz')
+      worker.send(:add_record_stats, 12345, 'active')
+    end
+  end
 
-  #   it "should add record stats for 'active'" do
-  #     collection_statistics.should_receive(:add_record!).with(12345, 'activated', "http://google.co.nz")
-  #     worker.send(:add_record_stats,12345, 'active')
-  #   end
-  # end
+  describe '#collection_statistics' do
+    let(:collection_statistics) { double(:collection_statistics).as_null_object }
+    let(:relation) { double(:relation) }
 
-  # describe "#collection_statistics" do
+    before do
+      worker.stub(:link_check_job) { link_check_job }
+      worker.instance_variable_set(:@link_check_job_id, link_check_job.id)
+    end
 
-  #   let(:collection_statistics) { double(:collection_statistics).as_null_object }
-  #   let(:relation) { double(:relation) }
+    it 'should find or create a collection statistics model with the collection_title' do
+      expect(CollectionStatistics).to receive(:find_or_create_by).with({day: Date.today, source_id: link_check_job.source_id}) { collection_statistics }
+      worker.send(:collection_stats).should eq collection_statistics
+    end
 
-  #   before do
-  #     worker.stub(:link_check_job) { link_check_job }
-  #     worker.instance_variable_set(:@link_check_job_id, link_check_job.id)
-  #   end
+    it 'memoizes the result' do
+      expect(CollectionStatistics).to receive(:find_or_create_by).once { collection_statistics }
+      worker.send(:collection_stats)
+    end
+  end
 
-  #   it "should find or create a collection statistics model with the collection_title" do
-  #     CollectionStatistics.should_receive(:find_or_create_by).with({day: Date.today, source_id: link_check_job.source_id}) { collection_statistics }
-  #     worker.send(:collection_stats).should eq collection_statistics
-  #   end
+  describe '#suppress_record' do
+    before { RestClient.stub(:put) }
 
-  #   it "memoizes the result" do
-  #     CollectionStatistics.should_receive(:find_or_create_by).once { collection_statistics }
-  #     worker.send(:collection_stats)
-  #     worker.send(:collection_stats)
-  #   end
-  # end
+    it 'should make a post to the api to change the status to supressed for the record' do
+      expect(RestClient).to receive(:put).with("#{ENV['API_HOST']}/harvester/records/abc123", {record: { status: 'suppressed' }})
+      worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 0)
+    end
 
+    it 'should trigger a new link_check_job with a strike of 1' do
+      expect(LinkCheckWorker).to receive(:perform_in).with(1.hours, link_check_job.id.to_s, 1)
+      worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 0)
+    end
 
+    it 'should not trigger a job after the third strike' do
+      expect(LinkCheckWorker).to_not receive(:perform_in)
+      worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 100)
+    end
 
+    it 'should not trigger a job on the third strike' do
+      expect(LinkCheckWorker).to_not receive(:perform_in)
+      worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 3)
+    end
 
-  # describe "#suppress_record" do
+    it 'should not send a request to set the record to suppressed if the strike is over 0 ' do
+      expect(worker).to_not receive(:set_record_status).with('abc123', "suppressed")
+      worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 1)
+    end
 
-  #   before { RestClient.stub(:put) }
+    context 'strike timings' do
+      it 'should perform the job in 1 hours on the 0th strike' do
+        expect(LinkCheckWorker).to receive(:perform_in).with(1.hours, link_check_job.id.to_s, 1)
+        worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 0)
+      end
 
-  #   it "should make a post to the api to change the status to supressed for the record" do
-  #     RestClient.should_receive(:put).with("#{ENV['API_HOST']}/harvester/records/abc123", {record: { status: 'suppressed' }})
-  #     worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 0)
-  #   end
+      it 'should perform the job in 5 hours on the 1th strike' do
+        expect(LinkCheckWorker).to receive(:perform_in).with(5.hours, link_check_job.id.to_s, 2)
+        worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 1)
+      end
 
-  #   it "should trigger a new link_check_job with a strike of 1" do
-  #     LinkCheckWorker.should_receive(:perform_in).with(1.hours, link_check_job.id.to_s, 1)
-  #     worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 0)
-  #   end
+      it 'should perform the job in 72 hours on the 2nd strike' do
+        expect(LinkCheckWorker).to receive(:perform_in).with(72.hours, link_check_job.id.to_s, 3)
+        worker.send(:suppress_record, link_check_job.id.to_s, 'abc123', 2)
+      end
+    end
 
-  #   it "should not trigger a job after the third strike" do
-  #     LinkCheckWorker.should_not_receive(:perform_in)
-  #     worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 100)
-  #   end
+    context 'strike three your out!' do
+      it 'should set the status of the record to deleted' do
+        expect(worker).to receive(:set_record_status).with('abc123', 'deleted')
+        worker.send(:suppress_record, link_check_job.id.to_s.to_s, 'abc123', 3)
+      end
+    end
+  end
 
-  #   it "should not trigger a job on the third strike" do
-  #     LinkCheckWorker.should_not_receive(:perform_in)
-  #     worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 3)
-  #   end
+  describe '#rules' do
+    before { link_check_job.stub_chain(:source, :_id) { 'abc123' } }
 
-  #   it "should not send a request to set the record to 'suppressed' if the strike is over 0 " do
-  #     worker.should_not_receive(:set_record_status).with("abc123", "suppressed")
-  #     worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 1)
-  #   end
-
-  #   context "strike timings" do
-  #     it "should perform the job in 1 hours on the 0th strike" do
-  #       LinkCheckWorker.should_receive(:perform_in).with(1.hours, link_check_job.id.to_s, 1)
-  #       worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 0)
-  #     end
-
-  #     it "should perform the job in 5 hours on the 1th strike" do
-  #       LinkCheckWorker.should_receive(:perform_in).with(5.hours, link_check_job.id.to_s, 2)
-  #       worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 1)
-  #     end
-
-  #     it "should perform the job in 72 hours on the 2nd strike" do
-  #       LinkCheckWorker.should_receive(:perform_in).with(72.hours, link_check_job.id.to_s, 3)
-  #       worker.send(:suppress_record, link_check_job.id.to_s, "abc123", 2)
-  #     end
-  #   end
-
-  #   context "strike three your out!" do
-  #     it "should set the status of the record to deleted" do
-  #       worker.should_receive(:set_record_status).with("abc123", "deleted")
-  #       worker.send(:suppress_record, link_check_job.id.to_s.to_s, "abc123", 3)
-  #     end
-  #   end
-  # end
-
-  # describe "set_record_status" do
-
-  #   before { RestClient.stub(:put) }
-
-  #   it "should make a post to the api to change the status to active for the record" do
-  #     RestClient.should_receive(:put).with("#{ENV['API_HOST']}/harvester/records/abc123", {record: { status: 'active' }})
-  #     worker.send(:set_record_status, "abc123", "active")
-  #   end
-
-  #   it "should add_record_stats" do
-  #     worker.should_receive(:add_record_stats).with(12345, "active")
-  #     worker.send(:set_record_status, 12345, "active")
-  #   end
-  # end
-
-  # describe "#rules" do
-  #   before do
-  #     link_check_job.stub_chain(:source, :_id) { 'abc123' }
-  #   end
-
-  #   it "should call link_check_rule with the source_id" do
-  #     worker.unstub(:rules)
-  #     worker.should_receive(:link_check_job) { link_check_job }
-  #     worker.should_receive(:link_check_rule).with('abc123') { link_check_rule }
-  #     worker.send(:rules).should eq link_check_rule
-  #   end
-  # end
-
+    it 'should call link_check_rule with the source_id' do
+      expect(worker).to receive(:link_check_job) { link_check_job }
+      expect(worker).to receive(:link_check_rule).with('abc123') { link_check_rule }
+      
+      worker.send(:rules).should eq link_check_rule
+    end
+  end
 end
