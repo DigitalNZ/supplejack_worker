e542bcf11d43abdc893f355f846adf44a190d930
Gustavo Motizuki <gustavo@boost.co.nz>
2017-08-31 16:14:04 +1200

    Handle SJ API create/delete request to proper propagate errors to the job

--- app/workers/api_delete_worker.rb
+++ app/workers/api_delete_worker.rb
@@ -4,14 +4,34 @@
 # 
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
+class ApiDeleteWorker < AbstractWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: 'default', retry: 5, backtrace: true
+  sidekiq_retry_in { 5.seconds }
+  sidekiq_retries_exhausted do |msg|
+    Sidekiq.logger.warn "Failed #{msg['class']} with #{msg['args']}: #{msg['error_message']}"
+    job_id = msg['args'].last
+    job = AbstractJob.find(job_id)
 
-class ApiDeleteWorker
-	include Sidekiq::Worker
-  sidekiq_options queue: 'default'
+    job.inc(posted_records_count: 1)
 
-	def perform(identifier, job_id)
-		job = AbstractJob.find(job_id)
-		RestClient.put("#{ENV["API_HOST"]}/harvester/records/delete", {id: identifier}, {content_type: :json, accept: :json})
-		job.inc(:posted_records_count => 1)
-	end
+    job.failed_records << FailedRecord.new(
+      exception_class: msg['class'],
+      message: msg['error_message'],
+      backtrace: msg['error_backtrace'],
+      raw_data: msg['args'].to_json
+    )
+  end
+
+  def perform(identifier, job_id)
+    @job_id = job_id
+    response = RestClient.put(
+      "#{ENV['API_HOST']}/harvester/records/delete",
+      { id: identifier },
+      content_type: :json, accept: :json
+    )
+    response = JSON.parse(response)
+
+    process_response(response)
+  end
 end
