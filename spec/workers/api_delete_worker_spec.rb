6ad1c8010600994ad75b2259866d638137d7952b
Gustavo Motizuki <gustavo@boost.co.nz>
2017-09-01 09:43:02 +1200

    Refactoring api update worker specs

--- spec/workers/api_delete_worker_spec.rb
+++ spec/workers/api_delete_worker_spec.rb
@@ -1,29 +1,92 @@
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3. 
-# See https://github.com/DigitalNZ/supplejack_worker for details. 
-# 
+# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
+# and is licensed under the GNU General Public License, version 3.
+# See https://github.com/DigitalNZ/supplejack_worker for details.
+#
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
-require "spec_helper"
+require 'spec_helper'
 
 describe ApiDeleteWorker do
-
   let(:worker) { ApiDeleteWorker.new }
+  let(:job) { FactoryGirl.create(:harvest_job) }
 
-  before do
-   RestClient.stub(:delete)
-   AbstractJob.stub(:find) { double(:job).as_null_object }
-  end
+  describe '#perform' do
+    let(:success_response) do
+      {
+        status: 'success',
+        record_id: 123
+      }
+    end
 
-  describe "#perform" do
+    let(:failed_response) do
+      {
+        status: 'failed',
+        exception_class: 'Exception',
+        message: 'Error message',
+        raw_data: 'some data',
+        record_id: 123
+      }
+    end
+
+    before(:each) do
+      AbstractJob.stub(:find).and_return(job)
+    end
+
     it 'is a default priority job' do
       expect(worker.sidekiq_options_hash['queue']).to eq 'default'
     end
-    
-    it "should send a put request to the api with the internal identifier" do
-      RestClient.should_receive(:put).with("#{ENV["API_HOST"]}/harvester/records/delete", {id: 'abc123'}, {content_type: :json, accept: :json})
-      worker.perform("abc123", "r3ec343")
+
+    it 'put attributes to the api' do
+      RestClient.should_receive(:put).and_return(success_response.to_json)
+      worker.perform('/harvester/records/123/fragments.json', {})
+    end
+
+    context 'API return a status: :failed' do
+      before(:each) do
+        RestClient.stub(:put).and_return(failed_response.to_json)
+      end
+
+      it 'increments job.posted_records_count' do
+        job.should_receive(:inc).with(posted_records_count: 1)
+        worker.perform('/harvester/records/123/fragments.json', {})
+      end
+
+      it 'updates job.last_posted_record_id' do
+        job.should_receive(:set).with(last_posted_record_id: 123)
+        worker.perform('/harvester/records/123/fragments.json', {})
+      end
+
+      it 'adds a FailedRecord to job.failed_records array' do
+        exception_class = failed_response[:exception_class]
+        message = failed_response[:message]
+        raw_data = failed_response[:raw_data]
+
+        worker.perform('/harvester/records/123/fragments.json', {})
+        failed = job.failed_records.first
+
+        expect(failed.attributes['exception_class']).to eq exception_class
+        expect(failed.attributes['message']).to eq message
+        expect(failed.attributes['raw_data']).to eq raw_data
+      end
+    end
+
+    context 'API return a status: :success' do
+      before(:each) do
+        RestClient.stub(:put).and_return(success_response.to_json)
+      end
+
+      it 'increments job.posted_records_count' do
+        job.should_receive(:inc).with(posted_records_count: 1)
+
+        worker.perform('/harvester/records/123/fragments.json', {})
+      end
+
+      it 'updates job.last_posted_record_id' do
+        job.should_receive(:set).with(last_posted_record_id: 123)
+
+        worker.perform('/harvester/records/123/fragments.json', {})
+      end
     end
   end
 end
