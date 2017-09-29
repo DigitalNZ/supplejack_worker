4f5546dec6429cea9b32fadcb8eceffa94582873
Reinkaos <richard@boost.co.nz>
2017-09-29 16:26:20 +1300

    Updates to show the retried record count

--- app/workers/abstract_worker.rb
+++ app/workers/abstract_worker.rb
@@ -47,6 +47,11 @@ class AbstractWorker
   def process_response(response)
     # raising an Exception will cause Sidekiq to retry the job.
     unless response['status'] == 'success'
+      # This scenario is because we cannot send an Array to the manager
+      # via Active Resource
+      job.retried_records << response['record_id']
+      job.save!
+      job.set(retried_records_count: job.retried_records.uniq.count)
       raise Supplejack::HarvestError.new(response['message'],
                                          response['backtrace'],
                                          response['raw_data'])
