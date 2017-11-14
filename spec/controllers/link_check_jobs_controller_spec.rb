d92a845d1c592597d87cd9a0ad80148015478975
Reinkaos <richard@boost.co.nz>
2017-11-15 10:44:09 +1300

    remove tapuhi related tests from link check jobs controller, link check rules controller, and enrichment worker

--- spec/controllers/link_check_jobs_controller_spec.rb
+++ spec/controllers/link_check_jobs_controller_spec.rb
@@ -1,22 +1,20 @@
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3. 
-# See https://github.com/DigitalNZ/supplejack_worker for details. 
-# 
+# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
+# and is licensed under the GNU General Public License, version 3.
+# See https://github.com/DigitalNZ/supplejack_worker for details.
+#
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
 require 'spec_helper'
 
 describe LinkCheckJobsController do
-
-  describe "POST create" do
+  describe 'POST create' do
     let(:link_check_job) { create(:link_check_job) }
 
-    it "should create a link_check_job" do
-      LinkCheckJob.should_receive(:create).with({'url' => 'http://google.co.nz', 'source_id' => 'tapuhi'}) { link_check_job }
-      post :create, { link_check: {url: "http://google.co.nz", source_id: 'tapuhi' } }
+    it 'should create a link_check_job' do
+      LinkCheckJob.should_receive(:create).with('url' => 'http://google.co.nz', 'source_id' => 'source_id') { link_check_job }
+      post :create, link_check: { url: 'http://google.co.nz', source_id: 'source_id' }
       assigns(:link_check).should eq link_check_job
     end
   end
-
 end
--- spec/controllers/link_check_rules_controller_spec.rb
+++ spec/controllers/link_check_rules_controller_spec.rb
@@ -1,14 +1,13 @@
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3. 
-# See https://github.com/DigitalNZ/supplejack_worker for details. 
-# 
+# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
+# and is licensed under the GNU General Public License, version 3.
+# See https://github.com/DigitalNZ/supplejack_worker for details.
+#
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
 require 'spec_helper'
 
 describe LinkCheckRulesController do
-
   let(:link_check_rule) { create(:link_check_rule) }
   let(:user) { create(:user) }
 
@@ -18,21 +17,21 @@ describe LinkCheckRulesController do
   end
 
   describe "GET 'index'" do
-    it "should get all of the collection rules" do
+    it 'should get all of the collection rules' do
       LinkCheckRule.should_receive(:all) { [link_check_rule] }
       get :index, {}, format: 'json'
       assigns(:link_check_rules).should eq [link_check_rule]
     end
 
-    it "should do a where if link_check_rule is defined" do
-      params = {link_check_rule: {collection_title: "TAPUHI"}}
+    it 'should do a where if link_check_rule is defined' do
+      params = { link_check_rule: { collection_title: 'link_check_rule' } }
       LinkCheckRule.should_receive(:where).with(params[:link_check_rule].stringify_keys)
       get :index, params, format: 'json'
     end
   end
 
   describe "GET 'show'" do
-    it "should get the collection rule" do
+    it 'should get the collection rule' do
       LinkCheckRule.should_receive(:find) { link_check_rule }
       get :show, id: link_check_rule.id
       assigns(:link_check_rule).should eq link_check_rule
@@ -40,9 +39,9 @@ describe LinkCheckRulesController do
   end
 
   describe "POST 'create'" do
-    it "should make a new collection rule and assign it" do
-      LinkCheckRule.should_receive(:create).with({ "collection_title" => "TAPUHI", "status_codes" => "203,205" }) { link_check_rule }
-      post :create, link_check_rule: { collection_title: "TAPUHI", status_codes: "203,205" }
+    it 'should make a new collection rule and assign it' do
+      LinkCheckRule.should_receive(:create).with('collection_title' => 'collection_title', 'status_codes' => '203,205') { link_check_rule }
+      post :create, link_check_rule: { collection_title: 'collection_title', status_codes: '203,205' }
       assigns(:link_check_rule) { link_check_rule }
     end
   end
@@ -50,14 +49,14 @@ describe LinkCheckRulesController do
   describe "PUT 'update'" do
     it "should find the link_check_rule" do
       LinkCheckRule.should_receive(:find) { link_check_rule }
-      put :update, id: link_check_rule.id, link_check_rule: { collection_title: "TAPUHI", status_codes: "203,205" }
+      put :update, id: link_check_rule.id, link_check_rule: { collection_title: 'collection_title', status_codes: '203,205' }
       assigns(:link_check_rule) { link_check_rule }
     end
 
     it "updates all the attributes" do
       LinkCheckRule.stub(:find) { link_check_rule }
-      link_check_rule.should_receive(:update_attributes).with({"collection_title" => "TAPUHI", "status_codes" => "203,205" })
-      put :update, id: link_check_rule.id, link_check_rule: { collection_title: "TAPUHI", status_codes: "203,205" }
+      link_check_rule.should_receive(:update_attributes).with('collection_title' => 'collection_title', 'status_codes' => '203,205')
+      put :update, id: link_check_rule.id, link_check_rule: { collection_title: 'collection_title', status_codes: '203,205' }
     end
   end
 
@@ -69,4 +68,3 @@ describe LinkCheckRulesController do
   end
 
 end
-
