c9a5fc304c18fb0150373a43a567279ca3acf507
Reinkaos <richard@boost.co.nz>
2017-11-15 10:56:24 +1300

    Replace tapuhi source id in favour of a simple string

--- test/factories/link_check_jobs.rb
+++ test/factories/link_check_jobs.rb
@@ -1,7 +1,7 @@
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government, 
-# and is licensed under the GNU General Public License, version 3. 
-# See https://github.com/DigitalNZ/supplejack_worker for details. 
-# 
+# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
+# and is licensed under the GNU General Public License, version 3.
+# See https://github.com/DigitalNZ/supplejack_worker for details.
+#
 # Supplejack was created by DigitalNZ at the National Library of NZ
 # and the Department of Internal Affairs. http://digitalnz.org/supplejack
 
@@ -11,6 +11,6 @@ FactoryGirl.define do
   factory :link_check_job do
     url "http://google.co.nz"
     record_id 123
-    source_id 'tapuhi'
+    source_id 'source_id'
   end
 end
