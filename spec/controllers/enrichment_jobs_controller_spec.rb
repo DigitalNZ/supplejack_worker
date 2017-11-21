4d680e980dbb31f85487167972efa041298dcf9b
Gustavo Motizuki <gustavo@boost.co.nz>
2017-11-22 12:18:48 +1300

    Remove devise and add Rails auth token instead

--- spec/controllers/enrichment_jobs_controller_spec.rb
+++ spec/controllers/enrichment_jobs_controller_spec.rb
@@ -12,49 +12,56 @@ require 'rails_helper'
 describe EnrichmentJobsController do
   let(:job) { double(:enrichment_job, save: true, update_attributes: true) }
 
-  before(:each) do
-    controller.stub(:authenticate_user!) { true }
-  end
-
-  describe 'POST create' do
-    it 'initializes a new enrichment job' do
-      post :create, params: { enrichment_job: { strategy: 'xml', file_name: 'youtube.rb' }, format: 'json' }
-      expect(assigns(:enrichment_job)).to be_a_new(EnrichmentJob)
-      expect(assigns(:enrichment_job).strategy).to eq 'xml'
-      expect(assigns(:enrichment_job).file_name).to eq 'youtube.rb'
-    end
-
-    it 'should save the enrichment job' do
-      EnrichmentJob.any_instance.should_receive(:save)
-      post :create, params: { enrichment_job: { strategy: 'xml', file_name: 'youtube.rb' }, format: 'json' }
-      expect(assigns(:enrichment_job).strategy).to eq 'xml'
-      expect(assigns(:enrichment_job).file_name).to eq 'youtube.rb'
-    end
-  end
-
-  describe '#GET show' do
-    it 'finds the enrichment job' do
-      EnrichmentJob.should_receive(:find).with('1') { job }
-      get :show, params: { id: 1 }, format: 'json'
-      expect(assigns(:enrichment_job)).to eq job
-    end
-  end
-
-  describe 'PUT Update' do
+  describe 'authorized requests' do
     before(:each) do
-      EnrichmentJob.stub(:find).with('1') { job }
+      request.headers['Authorization'] = "Token token=#{ENV['WORKER_KEY']}"
     end
 
-    it 'finds the enrichment job' do
-      EnrichmentJob.should_receive(:find).with('1') { job }
-      put :update, params: { id: 1, enrichment_job: { stop: true } }, format: 'json'
-      expect(assigns(:enrichment_job)).to eq job
+    describe 'POST create' do
+      it 'initializes a new enrichment job' do
+        post :create, params: { enrichment_job: { strategy: 'xml', file_name: 'youtube.rb' }, format: 'json' }
+        expect(assigns(:enrichment_job)).to be_a_new(EnrichmentJob)
+        expect(assigns(:enrichment_job).strategy).to eq 'xml'
+        expect(assigns(:enrichment_job).file_name).to eq 'youtube.rb'
+      end
+
+      it 'should save the enrichment job' do
+        EnrichmentJob.any_instance.should_receive(:save)
+        post :create, params: { enrichment_job: { strategy: 'xml', file_name: 'youtube.rb' }, format: 'json' }
+        expect(assigns(:enrichment_job).strategy).to eq 'xml'
+        expect(assigns(:enrichment_job).file_name).to eq 'youtube.rb'
+      end
     end
 
-    it 'should update the attributes' do
-      params = ActionController::Parameters.new(stop: 'true').permit!
-      job.should_receive(:update_attributes).with(params)
-      put :update, params: { id: 1, enrichment_job: { stop: true } }, format: 'json'
+    describe '#GET show' do
+      it 'finds the enrichment job' do
+        EnrichmentJob.should_receive(:find).with('1') { job }
+        get :show, params: { id: 1 }, format: 'json'
+        expect(assigns(:enrichment_job)).to eq job
+      end
     end
+
+    describe 'PUT Update' do
+      before(:each) do
+        EnrichmentJob.stub(:find).with('1') { job }
+      end
+
+      it 'finds the enrichment job' do
+        EnrichmentJob.should_receive(:find).with('1') { job }
+        put :update, params: { id: 1, enrichment_job: { stop: true } }, format: 'json'
+        expect(assigns(:enrichment_job)).to eq job
+      end
+
+      it 'should update the attributes' do
+        params = ActionController::Parameters.new(stop: 'true').permit!
+        job.should_receive(:update_attributes).with(params)
+        put :update, params: { id: 1, enrichment_job: { stop: true } }, format: 'json'
+      end
+    end
+  end
+
+  it 'prevent access if worker_key is not provided' do
+    get :show, params: { id: 1 }, format: 'json'
+    expect(response.status).to eq(401)
   end
 end
