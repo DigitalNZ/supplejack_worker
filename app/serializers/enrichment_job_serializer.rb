91d5e3be0da04479116824e4f5636be99377eb90
Theo Paul <theo.markkus.paul@gmail.com>
2018-02-22 14:38:27 +1300

    Remove Supplejack License text

--- app/serializers/enrichment_job_serializer.rb
+++ app/serializers/enrichment_job_serializer.rb
@@ -1,12 +1,4 @@
 # frozen_string_literal: true
-
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3.
-# See https://github.com/DigitalNZ/supplejack_worker for details.
-#
-# Supplejack was created by DigitalNZ at the National Library of NZ
-# and the Department of Internal Affairs. http://digitalnz.org/supplejack
-
 class EnrichmentJobSerializer < ActiveModel::Serializer
   attributes :id, :start_time, :end_time, :records_count, :throughput
   attributes :created_at, :duration, :status, :status_message, :user_id, :parser_id, :version_id, :environment, :enrichment
--- app/workers/api_delete_worker.rb
+++ app/workers/api_delete_worker.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
-
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government,
-# and is licensed under the GNU General Public License, version 3.
-# See https://github.com/DigitalNZ/supplejack_worker for details.
-#
-# Supplejack was created by DigitalNZ at the National Library of NZ
-# and the Department of Internal Affairs. http://digitalnz.org/supplejack
 class ApiDeleteWorker < AbstractWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'default', retry: 5, backtrace: true
