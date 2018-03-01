692f2ffe1e49879c1b0bd0bfbc8e35a10ff4468b
Theo Paul <theo.markkus.paul@gmail.com>
2018-03-01 16:14:22 +1300

    Surely last of licenses

--- app/workers/api_update_worker.rb
+++ app/workers/api_update_worker.rb
@@ -1,11 +1,5 @@
 # frozen_string_literal: true
 
-# The Supplejack Worker code is Crown copyright (C) 2014, New Zealand Government
-# and is licensed under the GNU General Public License, version 3.
-# See https://github.com/DigitalNZ/supplejack_worker for details.
-#
-# Supplejack was created by DigitalNZ at the National Library of NZ
-# and the Department of Internal Affairs. http://digitalnz.org/supplejack
 class ApiUpdateWorker < AbstractWorker
   include Sidekiq::Worker
   sidekiq_options queue: 'default', retry: 5, backtrace: true
