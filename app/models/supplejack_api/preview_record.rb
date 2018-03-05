cd928fe372f632456df775553f361c77d657eb3b
Oliver Stigley <oliver@boost.co.nz>
2018-03-05 13:44:53 +1300

    draft preview record changes for removing mongoid in enrichments

--- app/models/supplejack_api/preview_record.rb
+++ app/models/supplejack_api/preview_record.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
 module SupplejackApi
-  # app/models/supplejack_api/preview_record.rb
-  class PreviewRecord
+  class PreviewRecord < ActiveResource::Base
+    self.site = "#{ENV['API_HOST']}/harvester"
     include Enrichable
 
-    store_in collection: 'preview_records'
+    def self.find(query)
+      super(:all, params: { search: query, api_key: ENV['HARVESTER_API_KEY']})
+    end
   end
 end
