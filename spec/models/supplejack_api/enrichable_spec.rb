9177c6b202915376645a28a02c5b715989cb4456
Oliver Stigley <oliver@boost.co.nz>
2018-03-07 09:09:48 +1300

    fixed enrichable spec

--- spec/models/supplejack_api/enrichable_spec.rb
+++ spec/models/supplejack_api/enrichable_spec.rb
@@ -2,19 +2,26 @@
 require 'rails_helper'
 
 describe SupplejackApi::Enrichable do
-  let(:records) { SupplejackApi::Record.find('fragments.source_id' => 'digitalnz-sets') }
+  let(:records) { SupplejackApi::Record.find({ 'fragments.source_id' => 'digitalnz-sets' }, {page: 1}) }
 
   before do
-    records_response = [{
-      id: '5a81fa176a694240d94c9592',
-      fragments: [
-        { priority: 1, locations: %w[a b] },
-        { priority: 0, locations: %w[c d] }
-      ]
-    }].to_json
+    records_response = {
+      records: [{
+        id: '5a81fa176a694240d94c9592',
+        fragments: [
+          { priority: 1, locations: %w[a b] },
+          { priority: 0, locations: %w[c d] }
+        ]
+      }],
+      meta: {
+        page: 1,
+        total_pages: 1
+      }
+    }.to_json
 
     ActiveResource::HttpMock.respond_to do |mock|
-      mock.get "/harvester/records.json?api_key=#{ENV['HARVESTER_API_KEY']}&search%5Bfragments.source_id%5D=digitalnz-sets", {'Accept'=>'application/json'}, records_response, 201
+      url = "/harvester/records.json?api_key=#{ENV['HARVESTER_API_KEY']}&search%5Bfragments.source_id%5D=digitalnz-sets&search_options%5Bpage%5D=1"
+      mock.get(url, {'Accept'=>'application/json'}, records_response, 201)
     end
   end
 
@@ -37,9 +44,9 @@ describe SupplejackApi::Enrichable do
   end
 
   describe '#find' do
-    it 'should pass the api_key as a parameter' do
-      expect(ActiveResource::Base).to receive(:find).with(:all, params: { search: { 'fragments.source_id' => 'digitalnz-sets' }, api_key: ENV['HARVESTER_API_KEY'] })
-      SupplejackApi::Record.find('fragments.source_id' => 'digitalnz-sets')
+    it 'should pass the api_key and search options as parameters' do
+      expect(ActiveResource::Base).to receive(:find).with(:all, params: { search: { 'fragments.source_id' => 'digitalnz-sets' }, search_options: { page: 0 }, api_key: ENV['HARVESTER_API_KEY'] })
+      SupplejackApi::Record.find({'fragments.source_id' => 'digitalnz-sets'}, {page: 0})
     end
   end
 end
