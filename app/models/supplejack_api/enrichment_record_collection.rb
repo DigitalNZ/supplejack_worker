90dbf61ba74123e65de20962d131157c95eba598
Oliver Stigley <oliver@boost.co.nz>
2018-03-06 16:37:10 +1300

    rename active resource collection helper class

--- app/models/supplejack_api/enrichment_record_collection.rb
+++ app/models/supplejack_api/enrichment_record_collection.rb
@@ -2,6 +2,9 @@ module SupplejackApi
   class EnrichmentRecordCollection < ActiveResource::Collection
     attr_accessor :pagination
 
+    # This class lets us access the active resource response,
+    # so that can add pagination detail to the record object
+
     def initialize(elements = [])
       @elements = elements['records']
       @pagination = elements['meta']
