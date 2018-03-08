d00c431c746629682b47338cd4802c02ff8a4d1c
Gustavo Motizuki <gustavo@boost.co.nz>
2018-03-08 16:58:27 +1300

    Remove unused classes that were conflicting with active resource

--- app/models/supplejack_api/location.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-module SupplejackApi
-  class Location
-    include Mongoid::Document
-    include Mongoid::Attributes::Dynamic
-
-    embedded_in :fragment, class_name: 'SupplejackApi::ApiRecord::RecordFragment'
-  end
-end
