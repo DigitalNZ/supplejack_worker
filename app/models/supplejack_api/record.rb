a0f333a2ff85b965b7a7992763c282c155b75048
Oliver Stigley <oliver@boost.co.nz>
2018-05-30 11:04:52 +1200

    typo

--- app/models/supplejack_api/record.rb
+++ app/models/supplejack_api/record.rb
@@ -9,7 +9,7 @@ module SupplejackApi
     def self.find(query, page)
       super(:all, params: { search: query, search_options: page, api_key: ENV['HARVESTER_API_KEY'] })
     rescue ActiveResource::ServerError => e
-      Aibrake.notify(e, error_message: "The api request failed with: query: #{query} and page: #{page}.  #{e&.message}")
+      Airbrake.notify(e, error_message: "The api request failed with: query: #{query} and page: #{page}.  #{e&.message}")
       raise
     end
   end
