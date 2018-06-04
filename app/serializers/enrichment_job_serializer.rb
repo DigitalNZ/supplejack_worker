2ebf432d46f33359df75001a11b33cd8c547285a
Gustavo Motizuki <gustavo@boost.co.nz>
2018-06-05 10:34:46 +1200

    Add latest enrichment job record posted id

--- app/serializers/enrichment_job_serializer.rb
+++ app/serializers/enrichment_job_serializer.rb
@@ -4,7 +4,7 @@
 class EnrichmentJobSerializer < ActiveModel::Serializer
   attributes :id, :start_time, :end_time, :records_count, :throughput
   attributes :created_at, :duration, :status, :status_message, :user_id, :parser_id, :version_id, :environment, :enrichment
-  attributes :posted_records_count, :processed_count, :record_id
+  attributes :posted_records_count, :processed_count, :record_id, :last_posted_record_id
 
   # attribute starting with _ does not get serialized via attributes eg :_type
   # that's why we had to explicity define the attribute here
