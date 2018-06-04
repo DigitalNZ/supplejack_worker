96a93236094d33c67a8def22ae8576870ecde85b
Gustavo Motizuki <gustavo@boost.co.nz>
2018-06-05 09:52:01 +1200

    appease rubocop line length

--- app/serializers/harvest_job_serializer.rb
+++ app/serializers/harvest_job_serializer.rb
@@ -4,7 +4,8 @@
 class HarvestJobSerializer < ActiveModel::Serializer
   attributes :id, :start_time, :end_time, :records_count, :throughput
   attributes :created_at, :duration, :status, :status_message, :user_id, :parser_id, :version_id, :environment
-  attributes :failed_records_count, :invalid_records_count, :harvest_schedule_id, :mode, :posted_records_count, :retried_records_count, :last_posted_record_id
+  attributes :failed_records_count, :invalid_records_count, :harvest_schedule_id,
+             :mode, :posted_records_count, :retried_records_count, :last_posted_record_id
 
   # attribute starting with _ does not get serialized via attributes eg :_type
   # that's why we had to explicity define the attribute here
