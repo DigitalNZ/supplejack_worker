281823c29400a8c1c26ec5a9e99a2e0e130f92fc
Theo Paul <theo@boost.co.nz>
2019-02-21 14:00:38 +1300

    Add commented HTTP Auth boilerplate for Sidekiq dashboard

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -9,3 +9,12 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'] }
 end
+
+
+# # Uncommment if you need to add HTTP Auth to Sidekiq dashboard
+# require 'sidekiq'
+# require 'sidekiq/web'
+
+# Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
+#   [user, password] == ['username', 'password']
+# end
