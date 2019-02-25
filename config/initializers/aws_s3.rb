ca6a812aba3660ab562343da1094d8b0087e9a82
Tim Wright <tim@boost.co.nz>
2019-02-26 08:37:18 +1300

    store s3 in the config

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 if ENV['AWS_KEY'] && ENV['AWS_SECRET']
-  s3 = Aws::S3::Resource.new(region: 'ap-southeast-2', access_key_id: ENV['AWS_KEY'], secret_access_key: ENV['AWS_SECRET'])
+  config.s3 = Aws::S3::Resource.new(region: 'ap-southeast-2', access_key_id: ENV['AWS_KEY'], secret_access_key: ENV['AWS_SECRET'])
 else
   Rails.logger.debug 'The AWS_KEY and AWS_SECRET ENV Variables have not been set'
 end
