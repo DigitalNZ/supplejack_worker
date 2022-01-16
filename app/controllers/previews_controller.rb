d424b0ce8ff2d0132fb54962e47c7d8a73bc9bc8
Edwin Rozario <rozarioed@gmail.com>
2022-01-17 11:19:41 +1300

    feat(Preview Migration): preview data live in manager now

--- app/controllers/previews_controller.rb
+++ app/controllers/previews_controller.rb
@@ -1,22 +1,9 @@
 # frozen_string_literal: true
 
-# app/controllers/previews_controller.rb
 class PreviewsController < ApplicationController
   def create
-    @preview = Preview.spawn_preview_worker(preview_params)
-    render json: @preview
-  end
+    PreviewStartJob.perform_later(params[:preview][:id])
 
-  def show
-    @preview = Preview.find(params[:id])
-    render json: @preview
+    head :ok
   end
-
-  private
-    def preview_params
-      params.require(:preview).permit(:raw_data, :harvested_attributes, :api_record,
-                                      :status, :deletable, :field_errors, :validation_errors,
-                                      :harvest_failure, :harvest_job_errors,
-                                      :format, harvest_job: [:parser_code, :parser_id, :environment, :index, :limit, :user_id])
-    end
 end
