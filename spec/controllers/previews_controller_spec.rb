a2f9dd504bd3a6ac2bfff3e78a5344be2fbf09b9
Edwin Rozario <rozarioed@gmail.com>
2022-01-17 14:10:39 +1300

    test(PreviewsController): fixed for c1dfc0cbcf8afec6f91f6268eb212d2ade80607b

--- spec/controllers/previews_controller_spec.rb
+++ spec/controllers/previews_controller_spec.rb
@@ -3,24 +3,11 @@
 require 'rails_helper'
 
 describe PreviewsController do
-  let(:preview) { create(:preview, id: '123') }
-  let(:user) { create(:user, id: '1234') }
-
   describe 'POST create' do
-    let(:preview) { double(:preview) }
-    before { allow(Preview).to receive(:spawn_preview_worker).and_return('abc123') }
-
     it 'spawns a preview worker' do
-      allow(Preview).to receive(:spawn_preview_worker).with('id' => '123', 'harvest_job' => { 'parser_code' => 'CODE', 'index' => '1000' })
-      post :create, params: { preview: { id: '123', harvest_job: { parser_code: 'CODE', index: 1000 } } }
-    end
-  end
+      expect(PreviewStartJob).to receive(:perform_later).with('123')
 
-  describe 'GET show' do
-    it 'should find the preview object' do
-      allow(Preview).to receive(:find).with(preview.id.to_s).and_return(preview)
-      get :show, params: { id: preview.id }
-      expect(assigns(:preview)).to eq preview
+      post :create, params: { preview: { id: '123' } }
     end
   end
 end
