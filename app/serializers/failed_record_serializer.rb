8a391e8e5e0b3be73e30dcbe901b15cf559cbe9b
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2022-04-05 00:57:58 +0200

    style: made serializers attributes in rows to have nicer git history

--- app/serializers/failed_record_serializer.rb
+++ app/serializers/failed_record_serializer.rb
@@ -2,5 +2,11 @@
 
 # app/serializer/failed_record_serializer.rb
 class FailedRecordSerializer < ActiveModel::Serializer
-  attributes :exception_class, :message, :backtrace, :raw_data, :created_at
+  attributes %i[
+    exception_class
+    message
+    backtrace
+    raw_data
+    created_at
+  ]
 end
