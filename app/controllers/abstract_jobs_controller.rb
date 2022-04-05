498d344b6c4a2ab7f9b11b506f4a9718f1aedcbf
Edwin Rozario <rozarioed@gmail.com>
2022-04-05 15:01:04 +1200

    refactor(AbstractJobsController)

--- app/controllers/abstract_jobs_controller.rb
+++ app/controllers/abstract_jobs_controller.rb
@@ -7,26 +7,25 @@ class AbstractJobsController < ApplicationController
   def index
     @abstract_jobs = AbstractJob.search(search_params)
 
-    header_values = {
-      'X-total' => :total_count,
-      'X-offset' => :offset_value,
-      'X-limit' => :limit_value
-    }
-
-    header_values.each do |header, value|
+    headers_and_values_for_pagination.each do |header, value|
       response.headers[header] = @abstract_jobs.send(value).to_s
     end
-
 
     render json: @abstract_jobs
   end
 
-  # def jobs_since
-  #   render json: AbstractJob.jobs_since(search_params)
-  # end
+  def jobs_since
+    render json: AbstractJob.jobs_since(search_params)
+  end
 
   private
     def search_params
       params.permit(:status, :environment, :parser_id, :datetime, :page)
+    end
+
+    def headers_and_values_for_pagination
+      { 'X-total' => :total_count,
+        'X-offset' => :offset_value,
+        'X-limit' => :limit_value }
     end
 end
--- spec/controllers/abstract_jobs_controller_spec.rb
+++ spec/controllers/abstract_jobs_controller_spec.rb
@@ -4,32 +4,38 @@ require 'rails_helper'
 
 describe AbstractJobsController do
   describe 'GET index' do
-    before(:each) do
-      request.headers['Authorization'] = "Token token=#{ENV['WORKER_KEY']}"
-      mock_obj = double(total_count: 45, offset_value: 0, limit_value: 20)
-      allow(AbstractJob).to receive(:search).and_return(mock_obj)
+    context 'when worker_key is not provided' do
+      it 'returns unauthorized' do
+        get :index, params: { status: 'active' }
+
+        expect(response).to be_unauthorized
+      end
     end
 
-    it 'returns active abstract jobs' do
-      allow(AbstractJob).to receive(:search).with(
-        hash_including('status' => 'active')
-      ).and_call_original
+    context 'when worker_key is provided' do
+      before do
+        request.headers['Authorization'] = "Token token=#{ENV['WORKER_KEY']}"
 
-      get :index, params: { status: 'active' }
+        allow(AbstractJob)
+          .to receive(:search)
+          .and_return(double(total_count: 45, offset_value: 0, limit_value: 20))
+      end
+
+      it 'returns active abstract jobs' do
+        allow(AbstractJob).to receive(:search).with(
+          hash_including('status' => 'active')
+        ).and_call_original
+
+        get :index, params: { status: 'active' }
+      end
+
+      it 'should set pagination headers' do
+        get :index, params: { status: 'active' }
+
+        expect(response.headers['X-total']).to eq '45'
+        expect(response.headers['X-offset']).to eq '0'
+        expect(response.headers['X-limit']).to eq '20'
+      end
     end
-
-    it 'should set pagination headers' do
-      get :index, params: { status: 'active' }
-
-      expect(response.headers['X-total']).to eq '45'
-      expect(response.headers['X-offset']).to eq '0'
-      expect(response.headers['X-limit']).to eq '20'
-    end
-  end
-
-  it 'prevent access if worker_key is not provided' do
-    get :index, params: { status: 'active' }
-
-    expect(response.status).to eq(401)
   end
 end
