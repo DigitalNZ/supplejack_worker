54a058cc5ed728d6195eb0613e6e4688379d3e66
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2022-08-24 21:47:33 +0200

    conf: added session configuration for sidekiq

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,14 @@
 # frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
+# This also configures session_options for use below
 
-HarvesterWorker::Application.config.session_store :cookie_store, key: '_harvester_worker_session'
+Rails.application.configure do
+  config.session_store :cookie_store, key: '_harvester_worker_session'
+
+  # Required for all session management (regardless of session_store)
+  config.middleware.use ActionDispatch::Cookies
+  config.middleware.use config.session_store, config.session_options
+end
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
