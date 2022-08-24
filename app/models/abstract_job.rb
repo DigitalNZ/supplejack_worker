546c32bf52c706e8121583285a3aa59899d7132f
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2022-08-24 16:09:58 +0200

    refactor: created an abstract_job_serializer
    this avoids duplicated code in the harvest and enrichment serializers

--- app/models/abstract_job.rb
+++ app/models/abstract_job.rb
@@ -15,7 +15,6 @@ class AbstractJob
 
   field :start_time,            type: DateTime
   field :end_time,              type: DateTime
-  field :updated_at,            type: DateTime
   field :records_count,         type: Integer, default: 0
   field :processed_count,       type: Integer, default: 0
   field :throughput,            type: Float
