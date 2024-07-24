5b4eb6ac324e3fb86da0ef26eb6c87380be9b2c0
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-07-25 00:11:19 +0200

    conf: used the updated version to format logs in Sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,7 +5,7 @@ require 'ougai'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] }
-  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
+  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
 end
 
 Sidekiq.configure_client do |config|
